2520286ac8c249172ec5d0d44ad283e53e1e8f38
Abhishek Patil <patilabhishek394@gmail.com>
2025-07-10 13:16:10 +0530

    Changed design of footer of the portfolio.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,14 @@ import { Container } from "react-bootstrap";
 // Import the custom CSS for the Footer
 import "../style/Footer.css";
 
-function Footer() {
+export function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="footer-modern">
       <Container className="text-center">
         <p className="mb-0">
-          &copy; {currentYear} Your Name. Designed & Built with &hearts;
+          &copy; {currentYear} Abhishek Hanumant Patil. Designed & Built with
+          &hearts;
         </p>
       </Container>
     </footer>
